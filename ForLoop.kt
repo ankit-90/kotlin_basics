8daa7efb2981e2c019742c51f86bc674b5d5131a
Ankit Chandel <ankitchandel2@gmail.com>
2019-06-24 16:48:22 +0530

    Create ForLoop.kt

---
+++ ForLoop.kt
@@ -0,0 +1,27 @@
+fun main(){
+
+    //for loop with range
+    for (i in 1..10 ){
+        println(i)
+    }
+
+    //for loop with until
+    for (i in 1 until 10 ){
+        println(i)
+    }
+
+    //iterate in reverse order
+    for (i in 8 downTo 1){
+        println(i)
+    }
+
+    //it will print nothing
+    for (i in 5..1){
+        println(i)
+    }
+
+    for (i in 10 downTo  2   step 2){
+        println(i)
+    }
+
+}
